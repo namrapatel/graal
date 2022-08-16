ba404b0439301795bdf3b0278812309e750451ea
Namra Patel <namrajpatel@gmail.com>
2022-08-16 16:54:50 -0400

    commit client

--- src/components/CaptureableComponent.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity >=0.8.15;
-import "std-contracts/components/BoolComponent.sol";
-
-uint256 constant ID = uint256(keccak256("graal.component.Captureable"));
-
-contract CaptureableComponent is BoolComponent {
-  constructor(address world) BoolComponent(world, ID) {}
-}
