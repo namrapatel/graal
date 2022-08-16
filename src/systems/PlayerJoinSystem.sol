99af6546f461c99cf3dc848b96251fbc00cfdf2c
Namra Patel <namrajpatel@gmail.com>
2022-08-16 11:59:19 -0400

    refactor playerjoinsystem

--- src/systems/PlayerJoinSystem.sol
+++ src/systems/PlayerJoinSystem.sol
@@ -18,18 +18,19 @@ contract PlayerJoinSystem is System {
   constructor(IUint256Component _components, IWorld _world) System(_components, _world) {}
 
   function requirement(bytes memory arguments) public view returns (bytes memory) {
+    uint256 playerEntity = addressToEntity(msg.sender);
+
     PlayerComponent playerComponent = PlayerComponent(getAddressById(components, PlayerComponentID));
     require(!playerComponent.has(addressToEntity(msg.sender)), "Player already spawned");
 
-    return abi.encode(0); // TODO: Fix this to return the correct arguments
+    return abi.encode(playerComponent, playerEntity); 
   }
 
   function execute(bytes memory arguments) public returns (bytes memory) {
-    bytes memory temp = requirement(arguments);
+    (PlayerComponent playerComponent, uint256 playerEntity) = abi.decode(requirement(arguments), (PlayerComponent, uint256));
     
     // Create player entity
-    uint256 playerEntity = addressToEntity(msg.sender);
-    PlayerComponent(getAddressById(components, PlayerComponentID)).set(playerEntity);
+    playerComponent.set(playerEntity);
 
     // Spawn player in GraalCity
     LocationComponent(getAddressById(components, LocationComponentID)).set(playerEntity, uint32(Room.GraalCity));
