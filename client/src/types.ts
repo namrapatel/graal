9983fa21364f1231ca609747fe068430194d96f2
Namra Patel <namrajpatel@gmail.com>
2022-08-17 10:00:12 -0400

    add and clean client boilerplate

---
+++ client/src/types.ts
@@ -0,0 +1,6 @@
+import { boot } from "./boot";
+import { NetworkLayer } from "./layers/network";
+
+export type EmberWindow = Awaited<ReturnType<typeof boot>>;
+
+export type Layers = { network: NetworkLayer; };
